38af50950fe5567e4ca886086f929372c967f0eb
Xu Cao <48899968+msxuc@users.noreply.github.com>
2026-01-23 17:15:52 +0800

    vjp api redfine

--- easier/core/autodiff/utils.py
+++ easier/core/autodiff/utils.py
@@ -2,19 +2,186 @@
 # Licensed under the MIT License.
 
 
+import operator
 from types import EllipsisType
-from typing import Callable, Dict, TypeAlias, Union, cast
+from typing import Callable, Dict, Sequence, TypeAlias, Union, cast
 
 import torch
 from torch.fx import Graph, Node, GraphModule
 from torch.fx.node import BaseArgumentTypes as _FxConstBase
+from torch.fx.node import Argument as FxArg
 from torch.nn.modules import Module
 
-from easier.core.passes.utils import FX, EasierInterpreter, tree_map
+import easier.core.module as esr
+from easier.core.passes.tensor_grouping import get_node_tensor_group
+from easier.core.passes.utils import FX, EasierInterpreter, tree_map, \
+    fx_normalize_function_variant_into_kwargs, \
+    normalize_reducer_call_into_args, normalize_selector_call_into_args
+from easier.core.runtime.metadata import Role, RuntimeTensorMeta, \
+    get_node_meta, set_node_meta
 
 
 # e.g. int, float, dtype, device, slice, range, etc.
 FxConst: TypeAlias = Union[_FxConstBase, slice, range, EllipsisType, None]
+
+
+class PrimalMetaPropagator(EasierInterpreter):
+    def _fake_eval_meta_ctor(self, shape, dtype):
+        # Special function needed by get_value_runtime_info, to provide
+        # Role in TensorMeta.
+        ng = get_node_tensor_group(self.current_node)
+        if ng is None:
+            role = Role.REPLICATED
+
+        else:
+            role = Role.DISTRIBUTED
+            shape = (1000,) + shape[1:]
+
+        return RuntimeTensorMeta(role, shape, dtype)
+
+    def _create_zero_val(
+        self, raw_node_arg: Union[Node, Sequence[Node]]
+    ) -> Union[torch.Tensor, Sequence[torch.Tensor]]:
+        """
+        `raw_node_arg` is an argument of raw Graph Node, it may be a nested
+        structure,
+        e.g. torch.cat Node may have `args[0] == [x1, x2, x3]`.
+        
+        The result may also be a nested structure of many zero tensors.
+
+        Always on CPU.
+        """
+        def _make(x):
+            assert isinstance(x, Node), \
+                "In a list, Node and scalar are not expected to be mixed"
+
+            meta = get_node_meta(x)
+            assert isinstance(meta, RuntimeTensorMeta), \
+                "Value of arg Node cannot be nested structure"
+
+            return torch.zeros(meta.shape, dtype=meta.dtype)
+
+        return tree_map(raw_node_arg, _make)  # type: ignore
+    
+    def if_get_attr(self, submod_path: str, attr_name: str, attr_val) -> None:
+        # Avoid circle import
+        from easier.core.runtime.jit_engine.jit_engine import \
+            get_value_runtime_info
+        
+        runtime_meta = get_value_runtime_info(
+            self.current_node, attr_val, self._fake_eval_meta_ctor
+        )
+        set_node_meta(self.current_node, runtime_meta)
+    
+    def if_call_function(self, function: Callable) -> None:
+        if function is operator.getitem:
+            container, item_i = self.current_node.args
+            assert isinstance(container, Node)
+            imeta = get_node_meta(container)
+
+            if isinstance(imeta, Sequence):
+                assert isinstance(item_i, int)
+                item_meta = imeta[item_i]
+                set_node_meta(self.current_node, item_meta)
+                return
+        
+        self._handle_operation(function)
+        
+    def _handle_operation(self, function: Callable):
+        from easier.core.autodiff.autodiff_rule import \
+    Differentiability, RequiredParam, \
+    diff_rule_registry, differentiabilities
+
+        if function in diff_rule_registry:
+            rule_cls = diff_rule_registry[function]
+            rule = rule_cls(
+                self.current_node, function, self._fake_eval_meta_ctor
+            )
+
+            out_meta = rule.invoke_output_meta()
+            set_node_meta(self.current_node, out_meta)
+        
+        else:
+            if getattr(operator, function.__name__, None) is function:
+                if function is operator.truediv:
+                    function = torch.div  # torch does not have truediv
+                else:
+                    function = getattr(torch, function.__name__)
+
+            raw_node_normalized_kwargs: Dict[str, FxArg] = \
+                fx_normalize_function_variant_into_kwargs(
+                    function, self.current_node.args, self.current_node.kwargs
+                )  # type: ignore
+            dfbs = differentiabilities[function]
+            for dfb in dfbs:
+                if dfb.all_param_names() == set(
+                    raw_node_normalized_kwargs.keys()
+                ):
+                    break
+            else:
+                assert False, \
+                    "Failed to resolve overloading:" \
+                    f" with Differentiabilities {dfbs}," \
+                    f" got {raw_node_normalized_kwargs}"
+            
+            raw_node_diff_args: Dict[
+                str, Union[FxConst, Node, Sequence[Node]]
+            ] = {
+                p: raw_node_normalized_kwargs[p] for p in dfb.diffable_params
+            }  # type: ignore
+
+            kwvals = {
+                k: tree_map(
+                    v, self._create_zero_val
+                ) if isinstance(v, Node) else v
+                for k, v in raw_node_normalized_kwargs.items()
+            }
+            fake_res = function(**kwvals)
+            
+            from easier.core.runtime.jit_engine.jit_engine import \
+                get_value_runtime_info
+            out_meta = get_value_runtime_info(
+                self.current_node, fake_res, self._fake_eval_meta_ctor
+            )
+            set_node_meta(self.current_node, out_meta)
+        
+    def if_call_method(self, method_name: str):
+        function = getattr(torch.ops.aten, method_name)
+        self._handle_operation(function)
+
+
+    def if_call_module(self, submod: Module):
+        if isinstance(submod, esr.Module):
+            raise NotImplementedError()
+            # Nested easier.Module, must be JVP-ed.
+            # sub_jvp_transformer = JvpTransformer(self.root_jvp, submod).run()
+
+        # TODO make Selector/Reducer rules.
+
+        elif isinstance(submod, esr.Selector):
+            input = normalize_selector_call_into_args(
+                *self.current_node.args, **self.current_node.kwargs
+            )
+            assert isinstance(input, Node)
+            
+            # NOTE the fake batch size is not affected by S.idx.shape[0]
+            set_node_meta(self.current_node, get_node_meta(input))
+
+        elif isinstance(submod, esr.Reducer):
+            if submod.reduce != 'sum':
+                raise NotImplementedError()
+
+            input, out = normalize_reducer_call_into_args(
+                *self.current_node.args, **self.current_node.kwargs
+            )
+            assert isinstance(input, Node)
+            assert out is None or isinstance(out, Node)
+
+            # NOTE the fake batch size is not affected by S.idx.shape[0]
+            set_node_meta(self.current_node, get_node_meta(input))
+
+        else:
+            assert False, 'unreachable'
 
 
 class _TorchFuncGraphSimplifer(EasierInterpreter):
--- easier/core/autodiff/vjp.py
+++ easier/core/autodiff/vjp.py
@@ -17,9 +17,9 @@ from easier.core.passes.collective_initialization import \
 from easier.core.passes.tensor_grouping import \
     group_tensors, get_node_tensor_group
 from easier.core.passes.utils import \
-    EasierInterpreter, SubmodNameAllocator, normalize_reducer_call_into_args, \
+    FX, EasierInterpreter, SubmodNameAllocator, normalize_reducer_call_into_args, \
     fx_normalize_function_variant_into_kwargs, tree_map, \
-    normalize_selector_call_into_args
+    normalize_selector_call_into_args, get_attr_value
 
 from easier.core.runtime.metadata import \
     Role, RuntimeTensorMeta, StructuredTensorMeta, \
@@ -30,7 +30,7 @@ from easier.core.autodiff.jvp import Jvp
 from easier.core.autodiff.autodiff_rule import \
     Differentiability, RequiredParam, \
     diff_rule_registry, differentiabilities
-from easier.core.autodiff.utils import simplify_torchfunc_fx_graph, FxConst
+from easier.core.autodiff.utils import PrimalMetaPropagator, simplify_torchfunc_fx_graph, FxConst
 from easier.core.utils import EasierJitException
 
 
@@ -95,161 +95,6 @@ cotangent computation part for each raw Node, with `cotangents` parameter
 being replaced by the resultant cotangent of the previous VJP subgraph,
 in a sense of backpropagation.
 """
-
-
-class PrimalMetaPropagator(EasierInterpreter):
-    def _fake_eval_meta_ctor(self, shape, dtype):
-        # Special function needed by get_value_runtime_info, to provide
-        # Role in TensorMeta.
-        ng = get_node_tensor_group(self.current_node)
-        if ng is None:
-            role = Role.REPLICATED
-
-        else:
-            role = Role.DISTRIBUTED
-            shape = (1000,) + shape[1:]
-
-        return RuntimeTensorMeta(role, shape, dtype)
-
-    def _create_zero_val(
-        self, raw_node_arg: Union[Node, Sequence[Node]]
-    ) -> Union[torch.Tensor, Sequence[torch.Tensor]]:
-        """
-        `raw_node_arg` is an argument of raw Graph Node, it may be a nested
-        structure,
-        e.g. torch.cat Node may have `args[0] == [x1, x2, x3]`.
-        
-        The result may also be a nested structure of many zero tensors.
-
-        Always on CPU.
-        """
-        def _make(x):
-            assert isinstance(x, Node), \
-                "In a list, Node and scalar are not expected to be mixed"
-
-            meta = get_node_meta(x)
-            assert isinstance(meta, RuntimeTensorMeta), \
-                "Value of arg Node cannot be nested structure"
-
-            return torch.zeros(meta.shape, dtype=meta.dtype)
-
-        return tree_map(raw_node_arg, _make)  # type: ignore
-    
-    def if_get_attr(self, submod_path: str, attr_name: str, attr_val) -> None:
-        # Avoid circle import
-        from easier.core.runtime.jit_engine.jit_engine import \
-            get_value_runtime_info
-        
-        runtime_meta = get_value_runtime_info(
-            self.current_node, attr_val, self._fake_eval_meta_ctor
-        )
-        set_node_meta(self.current_node, runtime_meta)
-    
-    def if_call_function(self, function: Callable) -> None:
-        if function is operator.getitem:
-            container, item_i = self.current_node.args
-            assert isinstance(container, Node)
-            imeta = get_node_meta(container)
-
-            if isinstance(imeta, Sequence):
-                assert isinstance(item_i, int)
-                item_meta = imeta[item_i]
-                set_node_meta(self.current_node, item_meta)
-                return
-        
-        self._handle_operation(function)
-        
-    def _handle_operation(self, function: Callable):
-        if function in diff_rule_registry:
-            rule_cls = diff_rule_registry[function]
-            rule = rule_cls(
-                self.current_node, function, self._fake_eval_meta_ctor
-            )
-
-            out_meta = rule.invoke_output_meta()
-            set_node_meta(self.current_node, out_meta)
-        
-        else:
-            if getattr(operator, function.__name__, None) is function:
-                if function is operator.truediv:
-                    function = torch.div  # torch does not have truediv
-                else:
-                    function = getattr(torch, function.__name__)
-
-            raw_node_normalized_kwargs: Dict[str, FxArg] = \
-                fx_normalize_function_variant_into_kwargs(
-                    function, self.current_node.args, self.current_node.kwargs
-                )  # type: ignore
-            dfbs = differentiabilities[function]
-            for dfb in dfbs:
-                if dfb.all_param_names() == set(
-                    raw_node_normalized_kwargs.keys()
-                ):
-                    break
-            else:
-                assert False, \
-                    "Failed to resolve overloading:" \
-                    f" with Differentiabilities {dfbs}," \
-                    f" got {raw_node_normalized_kwargs}"
-            
-            raw_node_diff_args: Dict[
-                str, Union[FxConst, Node, Sequence[Node]]
-            ] = {
-                p: raw_node_normalized_kwargs[p] for p in dfb.diffable_params
-            }  # type: ignore
-
-            kwvals = {
-                k: tree_map(
-                    v, self._create_zero_val
-                ) if isinstance(v, Node) else v
-                for k, v in raw_node_normalized_kwargs.items()
-            }
-            fake_res = function(**kwvals)
-            
-            from easier.core.runtime.jit_engine.jit_engine import \
-                get_value_runtime_info
-            out_meta = get_value_runtime_info(
-                self.current_node, fake_res, self._fake_eval_meta_ctor
-            )
-            set_node_meta(self.current_node, out_meta)
-        
-    def if_call_method(self, method_name: str):
-        function = getattr(torch.ops.aten, method_name)
-        self._handle_operation(function)
-
-
-    def if_call_module(self, submod: Module):
-        if isinstance(submod, esr.Module):
-            raise NotImplementedError()
-            # Nested easier.Module, must be JVP-ed.
-            # sub_jvp_transformer = JvpTransformer(self.root_jvp, submod).run()
-
-        # TODO make Selector/Reducer rules.
-
-        elif isinstance(submod, esr.Selector):
-            input = normalize_selector_call_into_args(
-                *self.current_node.args, **self.current_node.kwargs
-            )
-            assert isinstance(input, Node)
-            
-            # NOTE the fake batch size is not affected by S.idx.shape[0]
-            set_node_meta(self.current_node, get_node_meta(input))
-
-        elif isinstance(submod, esr.Reducer):
-            if submod.reduce != 'sum':
-                raise NotImplementedError()
-
-            input, out = normalize_reducer_call_into_args(
-                *self.current_node.args, **self.current_node.kwargs
-            )
-            assert isinstance(input, Node)
-            assert out is None or isinstance(out, Node)
-
-            # NOTE the fake batch size is not affected by S.idx.shape[0]
-            set_node_meta(self.current_node, get_node_meta(input))
-
-        else:
-            assert False, 'unreachable'
 
 
 class VjpTransformer(EasierInterpreter):
@@ -403,6 +248,34 @@ class VjpTransformer(EasierInterpreter):
 
 
     def if_call_function(self, function: Callable) -> None:
+        if function is operator.setitem:
+            # A special syntactic Node for VJP to write the resultant primal
+            # Tensors.
+            container, index, value = self.current_node.args
+            assert isinstance(container, Node)
+
+            if index != Ellipsis:
+                raise NotImplementedError(
+                    "EASIER VJP currently only supports setitem with Ellipsis"
+                )
+            
+            if container.op != FX.GET_ATTR:
+                raise NotImplementedError(
+                    "EASIER VJP currently only supports setitem to"
+                    " GET_ATTR new_value Tensors"
+                )
+            
+            container_val = get_attr_value(self.raw_module, container)
+            if container_val not in self.vjp_module.outputs:
+                # outputs are subset of new_values, not all new_values are
+                # bound with cotangents.
+                raise EasierJitException(
+                    "Primal setitem can only write to output Tensors"
+                )
+                
+
+            return
+
         if function is operator.getitem:
             container = self.current_node.args[0]
             assert isinstance(container, Node)
@@ -508,9 +381,21 @@ class Vjp(Jvp):
 def vjp(
     module: esr.Module,
     inputs: Sequence[esr.Tensor],
+    new_values: Sequence[esr.Tensor],
     outputs: Sequence[esr.Tensor],
     vectors: Optional[Sequence[esr.Tensor]] = None
 ) -> Vjp:
+    """
+    Args:
+    -   inputs: primal input Tensors of `module`, cannot be written.
+    -   new_values:
+            output Tensors to store the primal output values,
+            cannot be overlapping with `inputs`.
+    -   outputs:
+            subset of `new_values`, cotangents for this subset of outputs
+            will be passed-in in the resultant VJP module.
+    -   vectors: User-defined cotangent Tensors for primal Tensors `outputs`.
+    """
 
     class _Vjp(Vjp):
         _raw_module_class = module.__class__.__qualname__
